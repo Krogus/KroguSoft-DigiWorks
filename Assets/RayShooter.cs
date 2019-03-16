460c30ed8e8e1574ed0e6c7070658c10acf0776b
Curiosity279 <nicolascoffey1@gmail.com>
2019-03-15 22:31:36 -0700

    Added RayShooter script(projectiles)

---
+++ Assets/RayShooter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera _camera;
+    // Use this for initialization
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Creates an indicator for shooting Rays
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 3 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, ((2 * _camera.pixelHeight) / 3), 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(SphereIndicator(hit.point));
+            }
+        }
+    }
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(2);
+        Destroy(sphere);
+    }
+}
