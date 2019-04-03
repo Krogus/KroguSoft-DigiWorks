f497f5018aedf567a65c0b6d8f94aae9b1eda0f9
Krogus <damonarthurbell@gmail.com>
2019-04-03 00:22:25 -0700

    Camera semi working !!
    Big thanks to big poppa

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -10,7 +10,7 @@ public class OrbitCamera : MonoBehaviour {
     private float _rotY;
     private float _rotX;
     private Vector3 _offset;
-
+    public Vector3 _testoffset;
 
     public float minimumVert = -45.0f; //these two floats will be used to stop the camera from going too high/low
     public float maximumVert = 45.0f;
@@ -39,27 +39,36 @@ public class OrbitCamera : MonoBehaviour {
 
     private float _rotationX = 0;
 
+    public float _testroty =0;
+
         //imagine this is kinda like a bottom bracket lol
 
 	// Use this for initialization
 	void Start () {
         gunX[0] = 0.5f; //x
-        gunX[1] = 0.8f; //y
+        gunX[1] = -1.0f; //y
         gunX[2] = - 1.0f; //z
 
-        gunY[0] = 0.0f;
-        gunY[1] = 0.0f;
-        gunY[2] = 0.0f;
+       // gunY[0] = 0.0f;
+       // gunY[1] = 0.0f;
+       // gunY[2] = 0.0f;
 
 
         _rotX = transform.eulerAngles.x; 
         _rotY = transform.eulerAngles.y;
         _offset = target.position - transform.position;
 
-	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
+         //public Vector3 _testoffset;
+        _testoffset.x = 0;
+        _testoffset.y = 0;
+        _testoffset.z = 10;
+
+
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
 
         if (Input.GetKeyDown(KeyCode.Mouse1))
 
@@ -75,28 +84,53 @@ public class OrbitCamera : MonoBehaviour {
         if (gunner == true)
         {
             //transform.position = target.position + (gunX + gunY);
-            transform.position = target.position + (gunX);
+            //transform.position = target.position + (gunX);
 
-            if (axes == RotationAxes.MouseX)
-            {
-                transform.Rotate(0, Input.GetAxis("Mouse Y") * sensitivityHor, 0);
-            }
-            else if (axes == RotationAxes.MouseY)
-            {
-                _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
-                _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
-            }
-            else
-            {
-                float rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityHor;
+            //_rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
 
-                _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
-                _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            //_rotX += Input.GetAxis("Mouse Y") * rotSpeed * 3;
 
-                transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
-            }
+            //_rotX = Mathf.Clamp(_rotX, minimumVert, maximumVert);
+
+            // Quaternion rotation = Quaternion.Euler(-_rotX, _rotY, 0);
+
+            _testroty= target.localEulerAngles.y+180;
+
+            Quaternion rotation = Quaternion.Euler(0, _testroty, 0);
+            Vector3 newtarget;
+            newtarget = target.position - (rotation * _testoffset);
+            
+
+           // newtarget.position = newtarget.position;// - (rotation * _testoffset);
+
+            //transform.position = target.position;// - (rotation * _offset);
+
+            transform.position = target.position;
+            transform.position = transform.position - (rotation * gunX);
+
+            transform.LookAt(newtarget);
 
 
+            /*      if (axes == RotationAxes.MouseX)
+                  {
+                      transform.Rotate(0, Input.GetAxis("Mouse Y") * sensitivityHor, 0);
+                  }
+                  else if (axes == RotationAxes.MouseY)
+                  {
+                      _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+                      _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+                  }
+                  else
+                  {
+                      float rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityHor;
+
+                      _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+                      _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+                      transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+                  }
+
+          */
 
 
 
